acddf11fdc2c1a8133d66bffd3a4956cc8c17b92
KenzieLippa <mckenzie.lippa1@marist.edu>
2022-09-08 14:40:55 -0400

    Update astroid.py

--- Asteroid_1/code/astroid.py
+++ Asteroid_1/code/astroid.py
@@ -18,6 +18,8 @@ print(ship_rect) #stores location
 background_surf = pygame.image.load('../graphics/background.png').convert()
 	#gets rid of of transparency without alpha#.. goes up a folder level from current
 #add a font object
+laser_surf = pygame.image.load('../graphics/laser.png').convert_alpha()
+laser_rect = laser_surf.get_rect(midbottom = ship_rect.midtop)
 font = pygame.font.Font('../graphics/subatomic.ttf', 50)#have a ttf for text
 text_surf = font.render('Space', True, 'White') #antialiasing softens the edges
 
@@ -46,17 +48,33 @@ while True:
 		if event.type == pygame.QUIT: #if the x button is pressed
 			pygame.quit()#quits the game
 			sys.exit() #ends any kind of code currently running, everything else will quit
-	
-	#framerate limit
-	clock.tick(120)		
+		'''can check the documentation to see what built in python apps are available'''
+	# 	if event.type == pygame.MOUSEMOTION:
+	# 		#each event has different attributes
+	# 		#print(event.pos) #which button are u pressing, rel is relative speed, and then pos is position
+	# 		# ship_rect.x = event.pos[0] - ship_rect.width/2
+	# 		# ship_rect.y = event.pos[1] - ship_rect.height/2
+	# 		#my attempt
+	# 		ship_rect.center = event.pos #take the center and make it th event position
+	# 	if event.type == pygame.MOUSEBUTTONDOWN:
+	# 		#releasing mouse button
+	# 		print(event.pos) #print where ur pressing th mouse down
+	# #framerate limit
+	clock.tick(120)	
+
+	#mouse input
+	print(pygame.mouse.get_pos()) # dont need an event for this
+	print(pygame.mouse.get_pressed()) #see what is pressed
+	ship_rect.center = pygame.mouse.get_pos()
+
 	#2. updates
 	display_surface.fill((0, 0, 0)) #can find the colors built in if you go through th documentation
 	#test_surf.fill((199, 154, 64))
 	#draws in order down
 	#need to fill whole surface each time or else ull get blurs
 	display_surface.blit(background_surf, (0,0))
-	if ship_rect.y > 0:
-		ship_rect.top -=4
+	# if ship_rect.y > 0:
+	# 	ship_rect.top -=4
 		#needs to be greater than cause it never actually equals 0
 
 	#if the top of th ship is at th top then stop
@@ -68,7 +86,9 @@ while True:
 	#can move any point u want and they will stay rellitive
 	#place the surface, surface and then position
 	#display_surface.blit(test_surf, (WINDOW_WIDTH - test_surf.get_width(),200))
+	display_surface.blit(laser_surf, laser_rect)
 
+	laser_rect.y -= 2
 	#3. show the frame to the player/update the display surface
 	pygame.display.update() #put all stuff on the window
 	#after closing pygame we dont technically quit th game so this bottom part still runs
